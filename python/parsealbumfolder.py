379b4be366457e9557b307117cfe27fc44e799f2
sizlo <t.brier93@gmail.com>
2016-09-08 07:46:24 +0100

    Keep all json data in one file, instead of a file for the album and an extra file per image

--- python/parsealbumfolder.py
+++ python/parsealbumfolder.py
@@ -9,19 +9,12 @@ def parseAlbumFolder(path):
     with open(path + "/album.json") as albumJsonFile:
         albumJsonData = json.load(albumJsonFile)
 
-    # Read all image%d%d%d.json files into images array
-    images = []
-    index = 1
-    imageJsonPath = "%s/image%03d.json" % (path, index)
-    while os.path.isfile(imageJsonPath):
-        with open(imageJsonPath) as imageJsonFile:
-            imageJsonData = json.load(imageJsonFile)
-            imageJsonData["path"] = path + "/" + imageJsonData["path"]
-            images.append(imageJsonData)
-
+    # Path stored for images in json is relative to the folder
+    # containing album.json, prepend this to each image path
+    index = 0
+    for imageJsonData in albumJsonData["images"]:
+        fullImagePath = path + "/" + imageJsonData["path"]
+        albumJsonData["images"][index]["path"] = fullImagePath
         index += 1
-        imageJsonPath = "%s/image%03d.json" % (path, index)
-
-    albumJsonData["images"] = images
 
     return albumJsonData
